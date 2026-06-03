109c05f0e9ce841e4733c501bed69cc37b8274f1
Sai9700128 <burra.sa@northeastern.edu>
2026-06-03 09:53:19 -0400

    Day 2 - Implemented tflint and Checkov

--- examples/sample.tf
+++ examples/sample.tf
@@ -1,3 +1,32 @@
+# Contains bad patterns to test the pipeline
+
 resource "aws_s3_bucket" "example" {
-  bucket = "my-test-bucket-12345"
+  bucket = "my-test-bucket"
+  acl    = "public-read" # Checkov: bucket publicly readable
 }
+
+resource "aws_db_instance" "prod" {
+  identifier          = "prod-db"
+  instance_class      = "db.t3.micro"
+  engine              = "mysql"
+  engine_version      = "8.0"
+  username            = "admin"
+  password            = "hardcoded_password_123" # Checkov: hardcoded secret
+  publicly_accessible = true                     # Checkov: exposed to internet
+  deletion_protection = false                    # Checkov: no deletion protection
+  storage_encrypted   = false                    # Checkov: no encryption
+  allocated_storage   = 20
+  skip_final_snapshot = true
+}
+
+resource "aws_security_group" "open" {
+  name = "open-sg"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Checkov: open to entire internet
+  }
+}
+
